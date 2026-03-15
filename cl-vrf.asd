dae40a878b5a0f85c9e8597320660be7234e9736
Park Ian Co <opensource@parkian.co>
2026-03-15 00:19:37 -0400

    Relicense from BSD-3-Clause to Apache-2.0

--- cl-vrf.asd
+++ cl-vrf.asd
@@ -7,7 +7,7 @@
   :description "Standalone Verifiable Random Functions (VRF) implementation"
   :version "0.1.0"
   :author "Parkian Company LLC"
-  :license "BSD-3-Clause"
+  :license "Apache-2.0"
   :depends-on ()
   :serial t
   :components ((:file "package")
--- test/test-vrf.lisp
+++ test/test-vrf.lisp
@@ -1,10 +1,10 @@
 ;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;; SPDX-License-Identifier: BSD-3-Clause
+;; SPDX-License-Identifier: Apache-2.0
 
 ;;;; test-vrf.lisp - Unit tests for vrf
 ;;;;
 ;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;;;; SPDX-License-Identifier: BSD-3-Clause
+;;;; SPDX-License-Identifier: Apache-2.0
 
 (defpackage #:cl-vrf.test
   (:use #:cl)
